3a8c9a4aeb64278a3121273ddb8d643e2f8434eb
Michael Emery <michael.emery@icloud.com>
2018-03-02 06:03:00 +1300

    refactor

--- checker/aliasing/Vanilla_CF.java
+++ checker/aliasing/Vanilla_CF.java
@@ -3,7 +3,7 @@ package aliasing;
 import org.checkerframework.common.aliasing.qual.Unique;
 
 /**
- * Inter-procedural corruption of an object via an alias.
+ * Intra-procedural corruption of an object via an alias.
  */
 public class Vanilla_CF {
 
@@ -13,29 +13,24 @@ public class Vanilla_CF {
         this.s = s;
     }
 
+    public Vanilla_CF(Vanilla_CF copy) {
+        this.s = copy.s;
+    }
+
     public static void main(String[] args) {
         @Unique Vanilla_CF foo = new Vanilla_CF("text");
         Vanilla_CF bar;
 
         // protect an object through cloning (correct)
-        bar = foo.makeCopy();
+        bar = new Vanilla_CF(foo);
         bar.s = null;
         System.out.println(foo.s.toString());  // "text"
 
         // corrupt an object via an alias (fail)
-        bar = foo.alias();
+        bar = foo;
         bar.s = null;
         System.out.println(foo.s.toString());  // NullPointerException
 
-    }
-
-    public Vanilla_CF makeCopy() {
-        Vanilla_CF copy = new Vanilla_CF(this.s);
-        return copy;
-    }
-
-    public Vanilla_CF alias() {
-        return this;
     }
 
 }
