ba935b78d9461af8675c3653e585baf768c106b7
Michael Emery <michael.emery@icloud.com>
2018-03-06 06:29:47 +1300

    edit readme

---
+++ checker/nullness/InvokeDynamic.java
@@ -0,0 +1,15 @@
+package nullness;
+
+import java.lang.reflect.Method;
+
+/**
+ * Assignment of a null reference via InvokeDynamic.
+ */
+public class InvokeDynamic {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
+
---
+++ checker/nullness/ReflectOverload.java
@@ -0,0 +1,14 @@
+package nullness;
+
+import java.lang.reflect.Method;
+
+/**
+ * Overloaded interprocedural assignment of a null reference via reflection.
+ */
+public class ReflectOverload {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
