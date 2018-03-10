4463103ab67089707fe3c2af958daf8cc736632c
Michael Emery <michael.emery@icloud.com>
2018-03-11 03:29:07 +1300

    template

---
+++ checker/nullness/InterproceduralReflectiveFieldAccess.java
@@ -0,0 +1,14 @@
+package nullness;
+
+import java.lang.reflect.Method;
+
+/**
+ * Assignment of a null reference to a field accessed *interprocedurally* via reflection.
+ */
+public class InterproceduralReflectiveFieldAccess {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
