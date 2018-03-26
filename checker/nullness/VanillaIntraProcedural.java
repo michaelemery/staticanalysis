3c2366a9cc53be39aacca6dbfe53ade9a0a577ba
Michael Emery <michael.emery@icloud.com>
2018-03-27 01:16:20 +1300

    Update VanillaIntraProcedural.java

--- checker/nullness/VanillaIntraProcedural.java
+++ checker/nullness/VanillaIntraProcedural.java
@@ -8,12 +8,12 @@ public class VanillaIntraProcedural {
     public static void main(String[] args) throws NullPointerException {
         String s;
 
-        // intraprocedural assignment of a non-null reference (correct)
-        s = "text";
+        // intraprocedural assignment of a non-null reference
+        s = "text";  // safe
         System.out.println(s.toString());  // "text"
 
-        // intraprocedural assignment of a null reference (fail)
-        s = null;
+        // intraprocedural assignment of a null reference
+        s = null;  // unsafe
         System.out.println(s.toString());  // NullPointerException
     }
 }
