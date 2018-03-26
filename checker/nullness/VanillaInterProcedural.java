e694ff30bf6d5156d9fbf5fe498387e3239edc21
Michael Emery <michael.emery@icloud.com>
2018-03-27 01:15:31 +1300

    Update VanillaInterProcedural.java

--- checker/nullness/VanillaInterProcedural.java
+++ checker/nullness/VanillaInterProcedural.java
@@ -8,14 +8,13 @@ public class VanillaInterProcedural {
     public static void main(String[] args) throws NullPointerException {
         String s;
 
-        // interprocedural assignment of a non-null reference (correct)
-        s = returnReceivedString("text");
+        // interprocedural assignment of a non-null reference
+        s = returnReceivedString("text");  // safe
         System.out.println(s.toString());  // "text"
 
         // interprocedural assignment of a null reference (fail)
-        s = returnReceivedString(null);
+        s = returnReceivedString(null);  // unsafe
         System.out.println(s.toString());  // NullPointerException
-
     }
 
     public static String returnReceivedString(String s) {
