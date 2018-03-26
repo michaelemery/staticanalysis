18e3bd3d76ccfa7f069e8881ba462dc65d70186a
Michael Emery <michael.emery@icloud.com>
2018-03-27 01:07:31 +1300

    Update ReflectMethodHandleIntraProcedural.java

--- checker/nullness/ReflectMethodHandleIntraProcedural.java
+++ checker/nullness/ReflectMethodHandleIntraProcedural.java
@@ -14,18 +14,18 @@ public class ReflectMethodHandleIntraProcedural {
         MethodHandles.Lookup lookup = MethodHandles.lookup();
         MethodHandle mh;
         String s;
-        
         Message message = new Message();
-        message.s = ""; // not null !
-        // get field with a non-null value (correct)
+            
+        // get field with a non-null value
+        message.s = "";
         mh = lookup.findGetter(Message.class, "s", String.class);
-        s = (String) mh.invoke(message);
+        s = (String) mh.invoke(message);  // safe
         System.out.println(s.toString());  // "text"
 
-        // get field with a null value (fail)
-        message.s = null; // null !
+        // get field with a null value
+        message.s = null;
         mh = lookup.findGetter(Message.class, "s", String.class);
-        s = (String) mh.invoke(message);
+        s = (String) mh.invoke(message);  // unsafe
         System.out.println(s.toString());  // NullPointerException
     }
 }
