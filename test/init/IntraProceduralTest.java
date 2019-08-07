3311806ca4fe15c9a924d3e3885afdc640347b78
Michael Emery <michael.emery@icloud.com>
2019-08-07 14:08:08 +1200

    removed redundant import

--- test/init/IntraProceduralTest.java
+++ test/init/IntraProceduralTest.java
@@ -1,7 +1,6 @@
 package init;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
