f69480ec17aa2caa4fba884ce07a73352d7ba843
Michael Emery <michael.emery@icloud.com>
2019-09-03 01:36:03 +1200

    refactor signedness

--- src/signedness/IntraProcedural.java
+++ src/signedness/IntraProcedural.java
@@ -1,43 +1,72 @@
 package signedness;
 
+import org.checkerframework.checker.signedness.qual.Signed;
 import org.checkerframework.checker.signedness.qual.Unsigned;
 
+import java.math.BigDecimal;
+
 /**
- *
+ * Check signedness of divisor set via direct value assignment.
  */
 class IntraProcedural {
 
-    int s1 = -2;
-    int s2 = -1;
+    // integral types are signed by default
 
-    @Unsigned int u1 = 2147483646; // unsigned: 2^32 - 2, signed: -2
-    @Unsigned int u2 = 2147483647; // unsigned: 2^32 - 1, signed: -1
+    public static void divisorSigned() throws Exception {
+        int s1 = -2;  // --> unsigned: 2^31 - 2
+        int s2 = -1;  // --> unsigned: 2^31 - 1
 
-    void m() {
-        int w = s1 / s2; // OK: result is 2, which is correct for -2 / -1
-        int x = u1 / u2; // ERROR: result is 2, which is incorrect for (2^32 - 2) / (2^32 - 1)
-        System.out.println(w);
-        System.out.println(x);
+        /*
+        correct answer is 2
+        test passes
+         */
+        if (s1 / s2 != 2) {
+            throw new Exception();
+        }
     }
 
-    int s3 = -1;
-    int s4 = 5;
+    public static void divisorUnsigned() throws Exception {
+        @Unsigned int u1 = 2147483646;  // = 2^31 - 2 --> signed: -2
+        @Unsigned int u2 = 2147483647;  // = 2^31 - 1 --> signed: -1
 
-    @Unsigned int u3 = 2147483647; // unsigned: 2^32 - 1, signed: -1
-    @Unsigned int u4 = 5;
+        /*
+        expected answer according to checkerframework documentation is 2
+        correct answer is 0 (rounded due to using int)
+        actual answer is 0 (= correct answer) therefore exception not thrown
+        test fails
+         */
+        if (u1 / u2 == 2) {
+            throw new Exception("Division by @Unsigned.");
+        }
+    }
 
-    void m2() {
-        int y = s3 % s4; // OK: result is -1, which is correct for -1 % 5
-        int z = u3 % u4; // ERROR: result is -1, which is incorrect for (2^32 - 1) % 5 = 2
-        System.out.println(y);
+    public static void modulusSigned() throws Exception {
+        int s1 = -1;
+        int s2 = 5;
+
+        /*
+        correct answer is -1
+        test passes
+         */
+        if (s1 % s2 != -1) {
+            throw new Exception();
+        }
+    }
+
+    public static void modulusUnsigned() throws Exception {
+        @Unsigned int u1 = 2147483647;  // = 2^31 - 1, signed: -1
+        @Unsigned int u2 = 5;
+
+        /*
+        expected answer according to checkerframework documentation is -1
+        correct answer is 2
+        actual answer is 2 (= correct answer) therefore exception not thrown
+        test fails
+         */
+        int z = u1 % u2;
         System.out.println(z);
-    }
-
-    public static void main(String[] args) {
-        IntraProcedural t1 = new IntraProcedural();
-        t1.m();
-        IntraProcedural t2 = new IntraProcedural();
-        t2.m2();
+        if (z == -1) {
+            throw new Exception("Division by @Unsigned.");
+        }
     }
 }
-
