78023020ed86e59133694c7357aeff77bce2c9b7
Michael Emery <michael.emery@icloud.com>
2019-09-03 01:45:15 +1200

    refactor signedness

---
+++ test/signedness/IntraProceduralTest.java
@@ -0,0 +1,41 @@
+package signedness;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Check signedness of divisor set via direct value assignment.
+ */
+class IntraProceduralTest {
+
+    @Test
+    void divisorSigned() {
+        assertDoesNotThrow(() -> {
+            IntraProcedural.divisorSigned();
+        });
+    }
+
+    @Test
+    void divisorUnsigned() {
+        Exception thrown = assertThrows(Exception.class, () -> {
+            IntraProcedural.divisorUnsigned();
+        });
+        assertTrue(thrown.getMessage().contains("Division by @Unsigned."));
+    }
+
+    @Test
+    void modulusSigned() {
+        assertDoesNotThrow(() -> {
+            IntraProcedural.modulusSigned();
+        });
+    }
+
+    @Test
+    void modulusUnsigned() {
+        Exception thrown = assertThrows(Exception.class, () -> {
+            IntraProcedural.modulusUnsigned();
+        });
+        assertTrue(thrown.getMessage().contains("Division by @Unsigned."));
+    }
+}
