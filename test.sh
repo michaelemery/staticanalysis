eafcdf9c5f2c98351e559f25d5756bac14897c02
Michael Emery <michael.emery@icloud.com>
2019-10-06 01:02:57 +1300

    fixed test script

--- test.sh
+++ test.sh
@@ -3,17 +3,5 @@ if [ -d "out" ]
 then
   rm -rf out/*
 fi
-if [ -n "$2" ]
-then
-  javac -d out/ -sourcepath src/ -cp /opt/junit-platform-console-standalone-1.5.2.jar test/$1/$2Test.java
-  java -jar /opt/junit-platform-console-standalone-1.5.2.jar --class-path out/ -c $1.$2Test
-else
-  if [ -n "$1" ]
-  then
-    javac -d out/ -sourcepath src/ -cp /opt/junit-platform-console-standalone-1.5.2.jar test/$1/*.java
-    java -jar /opt/junit-platform-console-standalone-1.5.2.jar --class-path out/ -p $1
-  else
-    javac -d out/ -sourcepath src/ -cp /opt/junit-platform-console-standalone-1.5.2.jar test/*/*Test.java
-    java -jar /opt/junit-platform-console-standalone-1.5.2.jar --class-path out/ --scan-class-path
-  fi
-fi
+javac -d out/ -sourcepath src/main/java -cp /opt/junit-platform-console-standalone-1.5.2.jar src/test/java/*/*Test.java
+java -jar /opt/junit-platform-console-standalone-1.5.2.jar --class-path out/ --scan-class-path
